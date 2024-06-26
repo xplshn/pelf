736f6f9a5e7534102355d6e3937f0346a82b4e7a
xplshn <114888778+xplshn@users.noreply.github.com>
2024-06-26 06:48:53 +0000

    Add files via upload

--- main.go
+++ main.go
@@ -23,14 +23,15 @@ type Options struct {
 	ProbeInterval     int      `json:"probe_interval"`
 	IconDir           string   `json:"icon_dir"`
 	AppDir            string   `json:"app_dir"`
+	ProbeExtensions   []string `json:"probe_extensions"`
 }
 
 type Config struct {
-	Options Options               `json:"options"`
-	Tracker map[string]*BlobEntry `json:"tracker"`
+	Options Options                 `json:"options"`
+	Tracker map[string]*BundleEntry `json:"tracker"`
 }
 
-type BlobEntry struct {
+type BundleEntry struct {
 	Path    string `json:"path"`
 	SHA     string `json:"sha"`
 	Png     string `json:"png,omitempty"`
@@ -39,7 +40,7 @@ type BlobEntry struct {
 }
 
 func main() {
-	log.Println("Starting Blapp daemon")
+	log.Println("Starting Nova daemon")
 
 	usr, err := user.Current()
 	if err != nil {
@@ -60,7 +61,7 @@ func main() {
 	probeInterval := time.Duration(config.Options.ProbeInterval) * time.Second
 
 	for {
-		processBlobs(config, usr.HomeDir)
+		processBundle(config, usr.HomeDir)
 		time.Sleep(probeInterval)
 	}
 }
@@ -72,8 +73,9 @@ func loadConfig(configPath, homeDir string) Config {
 			ProbeInterval:     90,
 			IconDir:           filepath.Join(homeDir, ".local/share/icons"),
 			AppDir:            filepath.Join(homeDir, ".local/share/applications"),
+			ProbeExtensions:   []string{".AppBundle", ".blob"},
 		},
-		Tracker: make(map[string]*BlobEntry),
+		Tracker: make(map[string]*BundleEntry),
 	}
 
 	file, err := os.Open(configPath)
@@ -109,7 +111,7 @@ func saveConfig(config Config, path string) {
 	}
 }
 
-func processBlobs(config Config, homeDir string) {
+func processBundle(config Config, homeDir string) {
 	existing := make(map[string]struct{})
 	options := config.Options
 	entries := config.Tracker
@@ -118,39 +120,43 @@ func processBlobs(config Config, homeDir string) {
 	for _, dir := range options.DirectoriesToWalk {
 		dir = strings.Replace(dir, "~", homeDir, 1)
 		log.Printf("Scanning directory: %s", dir)
-		blobs, err := filepath.Glob(filepath.Join(dir, "*.blob"))
-		if err != nil {
-			log.Printf("Failed to scan directory %s for .blob files: %v", dir, err)
-			continue
-		}
 
-		for _, blob := range blobs {
-			// VERBOSITY: log.Printf("Found blob: %s", blob)
-			existing[blob] = struct{}{}
+		for _, ext := range options.ProbeExtensions {
+			bundles, err := filepath.Glob(filepath.Join(dir, "*"+strings.ToLower(ext)))
+			if err != nil {
+				log.Printf("Failed to scan directory %s for %s files: %v", dir, ext, err)
+				continue
+			}
 
-			sha := computeSHA(blob)
-			if entry, checked := entries[blob]; checked {
-				if entry == nil {
-					// VERBOSITY: log.Printf("Entry for blob %s is nil, skipping", blob)
-					continue
-				}
+			for _, bundle := range bundles {
+				bundle = strings.ToLower(bundle)
+				// VERBOSITY: log.Printf("Found bundle: %s", bundle)
+				existing[bundle] = struct{}{}
 
-				if entry.SHA != sha {
-					if isExecutable(blob) {
-						processBlob(blob, sha, entries, options.IconDir, options.AppDir)
+				sha := computeSHA(bundle)
+				if entry, checked := entries[bundle]; checked {
+					if entry == nil {
+						// VERBOSITY: log.Printf("Entry for bundle %s is nil, skipping", bundle)
+						continue
+					}
+
+					if entry.SHA != sha {
+						if isExecutable(bundle) {
+							processBundles(bundle, sha, entries, options.IconDir, options.AppDir)
+							changed = true
+						} else {
+							// VERBOSITY: log.Printf("Blob is not executable: %s", blob)
+							entries[bundle] = nil
+						}
+					} // VERBOSITY: else {log.Printf("Blob already processed and unchanged: %s", blob)}
+				} else {
+					if isExecutable(bundle) {
+						processBundles(bundle, sha, entries, options.IconDir, options.AppDir)
 						changed = true
 					} else {
 						// VERBOSITY: log.Printf("Blob is not executable: %s", blob)
-						entries[blob] = nil
+						entries[bundle] = nil
 					}
-				} // VERBOSITY: else {log.Printf("Blob already processed and unchanged: %s", blob)}
-			} else {
-				if isExecutable(blob) {
-					processBlob(blob, sha, entries, options.IconDir, options.AppDir)
-					changed = true
-				} else {
-					// VERBOSITY: log.Printf("Blob is not executable: %s", blob)
-					entries[blob] = nil
 				}
 			}
 		}
@@ -158,8 +164,8 @@ func processBlobs(config Config, homeDir string) {
 
 	for path := range entries {
 		if _, found := existing[path]; !found {
-			log.Printf("Blob no longer exists: %s", path)
-			cleanupBlob(path, entries, options.IconDir, options.AppDir)
+			log.Printf("Bundle no longer exists: %s", path)
+			cleanupBundle(path, entries, options.IconDir, options.AppDir)
 			changed = true
 		}
 	}
@@ -197,30 +203,30 @@ func computeSHA(path string) string {
 	return hex.EncodeToString(hasher.Sum(nil))
 }
 
-func processBlob(path, sha string, entries map[string]*BlobEntry, iconPath, appPath string) {
-	entry := &BlobEntry{Path: path, SHA: sha}
+func processBundles(path, sha string, entries map[string]*BundleEntry, iconPath, appPath string) {
+	entry := &BundleEntry{Path: path, SHA: sha}
 
 	baseName := strings.TrimSuffix(filepath.Base(path), filepath.Ext(path))
 
-	entry.Png = executeBlob(path, "--pbundle_pngIcon", filepath.Join(iconPath, baseName+".png"))
-	entry.Xpm = executeBlob(path, "--pbundle_xpmIcon", filepath.Join(iconPath, baseName+".xpm"))
-	entry.Desktop = executeBlob(path, "--pbundle_desktop", filepath.Join(appPath, baseName+".desktop"))
+	entry.Png = executeBundle(path, "--pbundle_pngIcon", filepath.Join(iconPath, baseName+".png"))
+	entry.Xpm = executeBundle(path, "--pbundle_xpmIcon", filepath.Join(iconPath, baseName+".xpm"))
+	entry.Desktop = executeBundle(path, "--pbundle_desktop", filepath.Join(appPath, baseName+".desktop"))
 
 	if entry.Png != "" || entry.Xpm != "" || entry.Desktop != "" {
-		log.Printf("Adding blob to entries: %s", path)
+		log.Printf("Adding bundle to entries: %s", path)
 		entries[path] = entry
 	} else {
-		log.Printf("Blob does not contain required files: %s", path)
+		log.Printf("Bundle does not contain required files: %s", path)
 		entries[path] = nil
 	}
 }
 
-func executeBlob(blob, param, outputFile string) string {
+func executeBundle(blob, param, outputFile string) string {
 	log.Printf("Executing blob: %s with parameter: %s", blob, param)
 	cmd := exec.Command(blob, param)
 	output, err := cmd.Output()
 	if err != nil {
-		log.Printf("Blob %s with parameter %s returned error code 1", blob, param)
+		log.Printf("Bundle %s with parameter %s returned error code 1", blob, param)
 		return ""
 	}
 
@@ -240,7 +246,7 @@ func executeBlob(blob, param, outputFile string) string {
 	return outputFile
 }
 
-func cleanupBlob(path string, entries map[string]*BlobEntry, iconPath, appPath string) {
+func cleanupBundle(path string, entries map[string]*BundleEntry, iconPath, appPath string) {
 	if entry, found := entries[path]; found && entry != nil {
 		if entry.Png != "" {
 			os.Remove(entry.Png)
